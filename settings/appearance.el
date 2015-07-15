4dcd85333833d13cbc716ed4c0a21d1e28f7ca82
tiropp <florian.huber@desy.de>
2015-07-15 11:05:09 +0200

    1. remove setting of font. 2. Add function to remove the ^M from getting displayed

--- settings/appearance.el
+++ settings/appearance.el
@@ -6,7 +6,7 @@
 ;; SET DEFAULT FONT TO "Misc 7x13" ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;   you can get the font currently using by M-x describe-font
-(set-face-font 'default "-misc-fixed-medium-r-normal--13-*-*-*-c-70-iso8859-1")
+;; (set-face-font 'default "-misc-fixed-medium-r-normal--13-*-*-*-c-70-iso8859-1")
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -21,4 +21,13 @@
 (set-face-attribute 'region nil :background "#6699FF" :foreground "#000000")
 
 
-
+;;;;;;;;;;;;;;;;;;;;;;
+;; DON'T DISPLAY ^M ;;
+;;;;;;;;;;;;;;;;;;;;;;
+;; Taken from http://stackoverflow.com/questions/730751/hiding-m-in-emacs
+;; originally from Johan Bockgård
+(defun remove-dos-eol ()
+  "Do not show ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M []))
