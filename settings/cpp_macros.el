1c53a492492aa593ec7c975f74fded915abd435f
tiropp <tiropp@users.noreply.github.com>
2015-07-31 01:14:40 +0200

    Correct creation of menu-bar entry only for c++ mode

--- settings/cpp_macros.el
+++ settings/cpp_macros.el
@@ -1,20 +1,19 @@
 ;;
 ;; Define macros for C++
 ;;
-
-(defun insert-header(name)
+(defun cpp-insert-section-header(name)
   "Insert a code section header into c++ code"
   (interactive "sName: ")
   (let ((n (length name)))
     (let (( s1 (concat  "/****" (make-string n ?*) "****/\n") ))
-      (indent-according-to-mode)(insert s1)
-      (indent-according-to-mode)(insert "/*** " name " ***/\n")
-      (indent-according-to-mode)(insert s1)
-      )
+	 (indent-according-to-mode)(insert s1)
+	 (indent-according-to-mode)(insert "/*** " name " ***/\n")
+	 (indent-according-to-mode)(insert s1)
+	 )
     )
   )
 
-(defun insert-namespace(name)
+(defun cpp-insert-namespace(name)
   "Insert namespace snippet"
   (interactive "sName: ")
   (insert "namespace " name " {\n")
@@ -25,7 +24,7 @@
     )
   )
 
-(defun insert-guard(name)
+(defun cpp-insert-header-guard(name)
   "Insert header guard"
   (interactive "sName: ")
   (beginning-of-line)(insert "#ifndef " name "\n#define " name "\n\n")
@@ -35,29 +34,28 @@
     )
   )
 
-
 ;; Key map
-(define-key global-map [(control c) (control i) (h)] 'insert-header)
-(define-key global-map [(control c) (control i) (n)] 'insert-namespace)
-(define-key global-map [(control c) (control i) (g)] 'insert-guard)
-  
-
-;; Creating a new menu pane in the menu bar to the right of “Tools” menu
-(define-key-after
-  global-map
-  [menu-bar cpp_2]
-  (cons "C++2" (make-sparse-keymap "hoot hoot"))
-  'tools )
-;; Creating a menu item, under the menu by the id “[menu-bar mymenu]”
-(define-key
-  global-map
-  [menu-bar cpp_2 ih]
-  '("Insert Header" . insert-header))
+(define-key c-mode-base-map [(control n) (h)] 'cpp-insert-section-header)
+(define-key c-mode-base-map [(control n) (n)] 'cpp-insert-namespace)
+(define-key c-mode-base-map [(control n) (g)] 'cpp-insert-header-guard)
 
 
-;; Set the tab size to 4, same as indent size
-(setq tab-width 4)
+;; Creating a new menu pane 'C++2' in the menu bar
+(let ((menuMap (make-sparse-keymap "C++2")))  
+  (define-key c-mode-base-map [menu-bar cpp_2] (cons "C++2" menuMap))
+  (define-key menuMap [ih]
+    '("Insert Section Header" . cpp-insert-section-header))
+  (define-key menuMap [in]
+    '("Insert Namespace" . cpp-insert-namespace))
+  (define-key menuMap [ig]
+    '("Insert Header Guard" . cpp-insert-header-guard)))
+    
 
+(add-hook 'c-mode-common-hook
+	  ;; Set the tab size to 4, same as indent size
+	  (lambda()
+	    (setq tab-width 4)
+	    ))
 
 ;;;;;;;;;
 ;; OLD ;;
