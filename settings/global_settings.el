917f11bcc02efdd054604f7df88d44fff3911d00
tiropp <tiropp@users.noreply.github.com>
2015-07-31 17:25:44 +0200

    Add packages: volatile-highlight, undo-tree, yasnippet

--- settings/global_settings.el
+++ settings/global_settings.el
@@ -54,3 +54,37 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (global-set-key (kbd "\C-x x") 'copy-to-register)
 (global-set-key (kbd "\C-x g") 'insert-register)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; Volatile-Highlight ;;
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; From the original documentation:
+;;
+;; This library provides minor mode `volatile-highlight-mode', which
+;; brings visual feedback to some operations by highlighting portions
+;; relating to the operations.
+;;
+;; All of highlights made by this library will be removed
+;; when any new command is executed.
+;;
+(add-to-list 'load-path "~/.emacs.d/elpa/volatile-highlights-20141004.2240")
+(require 'volatile-highlights)
+(volatile-highlights-mode t)
+
+
+;;;;;;;;;;;;;;;
+;; Undo-Tree ;;
+;;;;;;;;;;;;;;;
+;; Use undo-tree-visualize to see the undo tree in a separated buffer.
+(add-to-list 'load-path "~/.emacs.d/elpa/undo-tree-20140509.522")
+(require 'undo-tree)
+
+
+;;;;;;;;;;;;;;;
+;; Yasnippet ;;
+;;;;;;;;;;;;;;;
+(add-to-list 'load-path "~/.emacs.d/elpa/yasnippet-20150415.244")
+(require 'yasnippet)
+(yas-global-mode 1)
+
