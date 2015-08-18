8d1ae8680b08dfc0d42bd84bd9ebde86a5b180a0
tiropp <florian.huber@desy.de>
2015-08-18 07:33:04 +0200

    Remove golden-radio usage

--- settings/global_settings.el
+++ settings/global_settings.el
@@ -128,38 +128,38 @@
 ;; smaller size that doesn't get in the way, but at the same time will be
 ;; readable enough to know it's content.
 ;;
-(add-to-list 'load-path "~/.emacs.d/elpa/golden-ratio-20150526.1200")
-(require 'golden-ratio)
+;; (add-to-list 'load-path "~/.emacs.d/elpa/golden-ratio-20150526.1200")
+;; (require 'golden-ratio)
 
-(add-to-list 'golden-ratio-exclude-modes "ediff-mode")
-(add-to-list 'golden-ratio-exclude-modes "helm-mode")
-(add-to-list 'golden-ratio-exclude-modes "dired-mode")
-(add-to-list 'golden-ratio-inhibit-functions 'pl/helm-alive-p)
+;; (add-to-list 'golden-ratio-exclude-modes "ediff-mode")
+;; (add-to-list 'golden-ratio-exclude-modes "helm-mode")
+;; (add-to-list 'golden-ratio-exclude-modes "dired-mode")
+;; (add-to-list 'golden-ratio-inhibit-functions 'pl/helm-alive-p)
 
-(defun pl/helm-alive-p ()
-  (if (boundp 'helm-alive-p)
-      (symbol-value 'helm-alive-p)))
+;; (defun pl/helm-alive-p ()
+;;   (if (boundp 'helm-alive-p)
+;;       (symbol-value 'helm-alive-p)))
 
-;; do not enable golden-raio in thses modes
-(setq golden-ratio-exclude-modes '("ediff-mode"
-                                   "gud-mode"
-                                   "gdb-locals-mode"
-                                   "gdb-registers-mode"
-                                   "gdb-breakpoints-mode"
-                                   "gdb-threads-mode"
-                                   "gdb-frames-mode"
-                                   "gdb-inferior-io-mode"
-                                   "gud-mode"
-                                   "gdb-inferior-io-mode"
-                                   "gdb-disassembly-mode"
-                                   "gdb-memory-mode"
-                                   "magit-log-mode"
-                                   "magit-reflog-mode"
-                                   "magit-status-mode"
-                                   "IELM"
-                                   "eshell-mode" "dired-mode"))
+;; ;; do not enable golden-raio in thses modes
+;; (setq golden-ratio-exclude-modes '("ediff-mode"
+;;                                    "gud-mode"
+;;                                    "gdb-locals-mode"
+;;                                    "gdb-registers-mode"
+;;                                    "gdb-breakpoints-mode"
+;;                                    "gdb-threads-mode"
+;;                                    "gdb-frames-mode"
+;;                                    "gdb-inferior-io-mode"
+;;                                    "gud-mode"
+;;                                    "gdb-inferior-io-mode"
+;;                                    "gdb-disassembly-mode"
+;;                                    "gdb-memory-mode"
+;;                                    "magit-log-mode"
+;;                                    "magit-reflog-mode"
+;;                                    "magit-status-mode"
+;;                                    "IELM"
+;;                                    "eshell-mode" "dired-mode"))
 
-(golden-ratio-mode)
+;; (golden-ratio-mode)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;
