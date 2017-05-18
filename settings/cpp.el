03f711b1cdfb25122da0f719f2183bf7a8231129
tiropp <florian.huber@desy.de>
2017-05-18 09:20:19 +0200

    add iedit package

--- settings/cpp.el
+++ settings/cpp.el
@@ -37,7 +37,31 @@
    :config
    (add-hook 'c-mode-common-hook (lambda () (interactive) (column-marker-3 80))))
   
-
+;; Multi cursor changes
+;;
+(use-package iedit
+  :ensure t
+  :defer t
+  :bind (("C-c m c" . iedit-mode))
+  :config  
+  (defun iedit-dwim (arg)
+    "Starts iedit but uses \\[narrow-to-defun] to limit its scope."
+    (interactive "P")
+    (if arg
+	(iedit-mode)
+      (save-excursion
+	(save-restriction
+	  (widen)
+	  ;; this function determines the scope of `iedit-start'.
+	  (if iedit-mode
+	      (iedit-done)
+	    ;; `current-word' can of course be replaced by other
+	    ;; functions.
+	    (narrow-to-defun)
+	    (iedit-start (current-word) (point-min) (point-max)))))))
+  (global-set-key (kbd "C-c m d") 'iedit-dwim)
+  )
+					   
 ;; Activate spell checking within comments
 (add-hook 'c-mode-common-hook 'flyspell-prog-mode)
 (add-hook 'c-mode-common
