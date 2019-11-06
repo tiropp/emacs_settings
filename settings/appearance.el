60132569cc92ed26f58202cd97a1d6d694fc7c19
tiropp <florian.huber@desy.de>
2019-11-06 21:40:46 +0100

    add my own powerline theme
    (git-commit-buffer-message)

--- settings/appearance.el
+++ settings/appearance.el
@@ -50,9 +50,80 @@
 ;;;;;;;;;;;;;;;;
 ;; BOTTOM BAR ;;
 ;;;;;;;;;;;;;;;;
+;;
+;; POWERLINE
+;;
+;; This theme is based on powerline-default-them
+;;
+;; The only change basically is that the line/column numbers are moved to the
+;; left side, since when the window is too small one cannot see them anymore.
+;;
+(defun powerline-my-theme ()
+  "Setup the default mode-line."
+  (interactive)
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (powerline-selected-window-active))
+                          (mode-line-buffer-id (if active 'mode-line-buffer-id 'mode-line-buffer-id-inactive))
+                          (mode-line (if active 'mode-line 'mode-line-inactive))
+                          (face0 (if active 'powerline-active0 'powerline-inactive0))
+                          (face1 (if active 'powerline-active1 'powerline-inactive1))
+                          (face2 (if active 'powerline-active2 'powerline-inactive2))
+                          (separator-left (intern (format "powerline-%s-%s"
+                                                          (powerline-current-separator)
+                                                          (car powerline-default-separator-dir))))
+                          (separator-right (intern (format "powerline-%s-%s"
+                                                           (powerline-current-separator)
+                                                           (cdr powerline-default-separator-dir))))
+                          (lhs (list (powerline-raw "%*" face0 'l)
+                                     (when powerline-display-buffer-size
+                                       (powerline-buffer-size face0 'l))
+                                     (when powerline-display-mule-info
+                                       (powerline-raw mode-line-mule-info face0 'l))
+                                     (powerline-buffer-id `(mode-line-buffer-id ,face0) 'l)
+
+				     ;; line/column numbers
+                                     (unless window-system
+                                       (powerline-raw (char-to-string #xe0a1) face0 'l))
+                                     (powerline-raw "%l" face0 'l)
+                                     (powerline-raw ":" face0)
+                                     (powerline-raw "%c" face0)
+
+				     ;; "arrow"
+                                     (when (and (boundp 'which-func-mode) which-func-mode)
+                                       (powerline-raw which-func-format face0 'l))
+                                     (powerline-raw " " face0)
+				     (funcall separator-left face0 face1)
+                                     (when (and (boundp 'erc-track-minor-mode) erc-track-minor-mode)
+                                       (powerline-raw erc-modified-channels-object face1 'l))
+
+				     ;; modes
+                                     (powerline-major-mode face1 'l)
+                                     (powerline-process face1)
+                                     (powerline-minor-modes face1 'l)
+                                     (powerline-narrow face1 'l)
+                                     (powerline-raw " " face1)
+                                     (funcall separator-left face1 face2)
+                                     (powerline-vc face2 'r)
+                                     (when (bound-and-true-p nyan-mode)
+                                       (powerline-raw (list (nyan-create)) face2 'l))))
+                          (rhs (list (powerline-raw global-mode-string face2 'r)
+                                     (funcall separator-right face2 face1)
+                                     (funcall separator-right face1 face0)
+                                     (powerline-raw " " face0)
+                                     (powerline-raw "%6p" face0 'r)
+                                     (when powerline-display-hud
+                                       (powerline-hud face0 face2))
+                                     (powerline-fill face0 0)
+                                     )))
+                     (concat (powerline-render lhs)
+                             (powerline-fill face2 (powerline-width rhs))
+                             (powerline-render rhs)))))))
+
 (use-package powerline
   :ensure t
-  :init (powerline-default-theme)
+  :init (powerline-my-theme)
   )
 
 
