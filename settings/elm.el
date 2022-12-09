1721aae002f6d0a171dbb6a63cfb9b9174fcd616
Huber, Florian <florian.huber@tiropp.ch>
2022-12-09 18:30:27 +0100

    exclude elm, due to conflict with lsp

--- settings/elm.el
+++ settings/elm.el
@@ -1,5 +1,9 @@
-(use-package elm-mode
-  :ensure t
-  :config
-  (setq elm-format-on-save t)
-  )
+;; Currently turn off elm-mode because there is some problem with project-root
+;; function causing lsp-mode to fail.
+;; See https://github.com/emacs-lsp/lsp-mode/issues/2335
+;;
+;; (use-package elm-mode
+;;   :ensure t
+;;   :config
+;;   (setq elm-format-on-save t)
+;;   )
