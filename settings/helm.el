1fcabf03807ed36a237fbbd748aaa24031708463
tiropp <florian.huber@desy.de>
2023-01-08 00:16:13 +0100

    remove packages which don't exist anymore

--- settings/helm.el
+++ settings/helm.el
@@ -56,8 +56,6 @@
 	 )
    
    :config
-   (require 'helm-config)
-
    ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
    ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
    ;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
--- settings/programming.el
+++ settings/programming.el
@@ -484,10 +484,6 @@
     :ensure t
     :commands lsp-ui-mode
     )
-  (use-package company-lsp
-    :ensure t
-    :commands company-lsp
-    )
   (use-package helm-lsp
     :ensure t
     :commands helm-lsp-workspace-symbol
