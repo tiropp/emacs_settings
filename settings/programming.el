34936ab73092b8515ea595df363dc9d26820cf90
Florian Huber <florian.huber@bernina.com>
2024-05-17 15:02:42 +0200

    improve clojure setup

--- settings/programming.el
+++ settings/programming.el
@@ -830,3 +830,104 @@
   :custom
   ;; rust-analyzer taken from rustup apt package
   (rustic-analyzer-command '("rustup" "run" "stable" "rust-analyzer")))
+
+
+;;;;;;;;;;;;;
+;; Clojure ;;
+;;;;;;;;;;;;;
+(use-package paredit
+  :ensure t)
+
+(use-package rainbow-delimiters
+  :ensure t)
+
+;; Some of the setup is taken from Daniel Higginbotham's clojure setup in
+;; "Clojure for the Branve and True" book, see config file at [1].
+;;
+;; [1] https://github.com/flyingmachine/emacs-for-clojure/archive/book1.zip
+(use-package clojure-mode
+  :ensure t
+  :hook (clojure-mode .
+	 (lambda ()
+	   ;; Enable paredit for Clojure
+	   (enable-paredit-mode)
+
+	   ;; This is useful for working with camel-case tokens, like names of
+	   ;; Java classes (e.g. JavaClassName)
+	   (subword-mode)
+
+	   ;; syntax hilighting for midje
+	   (setq inferior-lisp-program "lein repl")
+	   (font-lock-add-keywords
+	    nil
+	    '(("(\\(facts?\\)"
+	       (1 font-lock-keyword-face))
+	      ("(\\(background?\\)"
+	       (1 font-lock-keyword-face))))
+	   (define-clojure-indent (fact 1))
+	   (define-clojure-indent (facts 1))
+	   (rainbow-delimiters-mode))))
+
+;; A little more syntax highlighting
+(use-package clojure-mode-extra-font-locking
+  :ensure t)
+
+;;
+;; CIDER (Clojure interactice development environment that rocks)
+;;
+;; these help me out with the way I usually develop web apps
+(defun cider-start-http-server ()
+  (interactive)
+  (cider-load-current-buffer)
+  (let ((ns (cider-current-ns)))
+    (cider-repl-set-ns ns)
+    (cider-interactive-eval (format "(println '(def server (%s/start))) (println 'server)" ns))
+    (cider-interactive-eval (format "(def server (%s/start)) (println server)" ns))))
+
+
+(defun cider-refresh ()
+  (interactive)
+  (cider-interactive-eval (format "(user/reset)")))
+
+(defun cider-user-ns ()
+  (interactive)
+  (cider-repl-set-ns "user"))
+
+(use-package cider
+  :ensure t
+  :hook (
+	 ;; provides minibuffer documentation for the code you're typing into the repl
+	 (cider-mode . eldoc-mode)
+
+	 ;; enable paredit in your REPL
+	 (cider-repl-mode . paredit-mode))
+
+  :config
+  ;; go right to the REPL buffer when it's finished connecting
+  (setq cider-repl-pop-to-buffer-on-connect t)
+
+  ;; When there's a cider error, show its buffer and switch to it
+  (setq cider-show-error-buffer t)
+  (setq cider-auto-select-error-buffer t)
+
+  ;; Where to store the cider history.
+  (setq cider-repl-history-file "~/.emacs.d/cider-history")
+
+  ;; Wrap when navigating history.
+  (setq cider-repl-wrap-history t)
+
+  ;; Use clojure mode for other extensions
+  (add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
+  (add-to-list 'auto-mode-alist '("\\.boot$" . clojure-mode))
+  (add-to-list 'auto-mode-alist '("\\.cljs.*$" . clojure-mode))
+  (add-to-list 'auto-mode-alist '("lein-env" . enh-ruby-mode))
+
+  :bind (:map clojure-mode-map
+	 ("C-c C-v" . cider-start-http-server)
+	 ("C-M-r" . cider-refresh)
+	 ("C-c u" . cider-user-ns)
+	 :map cider-mode-map
+	 ("C-c u" . cider-user-ns)))
+
+(use-package inf-clojure
+  :ensure t)
